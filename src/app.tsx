c88c3550b8a9c8f776b6e72dce7e8c1ad5b1efd2
LimMem <qin.chuanlong@iwhalecloud.com>
2022-06-24 17:59:24 +0800

    修改局部方法

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,4 @@
 import {
-  RequestConfig,
   NavBarProps,
   TitleListItem,
   NavBarListItem,
@@ -8,123 +7,12 @@ import {
   setPageNavBar,
   history,
 } from 'alita';
-import {
-  lcdpApi,
-  initBasicConfig,
-  renderBefore,
-  authLoginService,
-  getKeepAlive,
-  queryDynamicRoutesService,
-} from '@lingxiteam/engine-mobile';
+import { initBasicConfig } from '@lingxiteam/engine-mobile';
 
 initBasicConfig({
   history,
   setPageNavBar,
 });
-
-// const login = {
-//   success: async () => {
-//     try {
-//       const e = (await authLoginService()) as any;
-//       const { loginInfo = {} } = e?.resultObject || {};
-//       lcdpApi.setData('user', loginInfo || {});
-//       history.push({
-//         pathname: '/',
-//       });
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   },
-//   updateRoutes: async (routes: any[], next: (arg0: any[]) => void) => {
-//     const dynamicRoutes = await queryDynamicRoutesService();
-//     const targetRoutes = merge(routes, dynamicRoutes);
-//     routes[0].routes = targetRoutes;
-//     next(routes);
-//   },
-// };
-
-// const auth = {
-//   fail: () => {
-//     // saveRedirctUrl(history?.location?.pathname, history?.location?.query);
-//     if (history) {
-//       history.push({
-//         pathname: '/login',
-//       });
-//     }
-//   },
-//   success: (e: { resultObject: { loginInfo?: {} | undefined } }) => {
-//     const { loginInfo = {} } = e?.resultObject;
-//     lcdpApi.setData('user', loginInfo || {});
-//   },
-// };
-
-// const page = {
-//   onListener: (pageData: any) => {
-//     const pageTitle = document.querySelector('.alita-layout-head .am-navbar-title');
-//     if (pageTitle) {
-//       pageTitle.innerHTML = pageData.pageName;
-//     }
-//   },
-// };
-
-// lcdpApi.handleMessage = {
-//   login,
-//   auth,
-//   page,
-// };
-
-const merge = (routes: any[], dynamicRoutes: any[]) => {
-  let targetRoutes: any[] = [];
-  if (routes.length > 0) {
-    targetRoutes = routes[0].routes || [];
-  }
-  dynamicRoutes.forEach((route) => {
-    // 页面路由 > 动态路由 > 内置路由
-    const repeatRoute = targetRoutes.find((t) => t.path === route.path);
-    if (!repeatRoute) {
-      targetRoutes.push(route);
-    }
-  });
-  return targetRoutes;
-};
-
-let dRoutes: any[] = [];
-// export function patchRoutes({ routes }: any) {
-//   let targetRoutes = merge(routes, dRoutes);
-//   const homePath = targetRoutes.find((item) => item.path === '/index');
-//   // const hasHomePath = extraRoutes.filter(item => item.path === '/index');
-//   if (homePath) {
-//     targetRoutes = targetRoutes.map((item: any) => {
-//       if (item.path === '/') {
-//         return { ...homePath, path: item.path };
-//       }
-//       return item;
-//     });
-//   }
-//   // eslint-disable-next-line no-param-reassign
-//   routes[0].routes = targetRoutes;
-// }
-
-// export async function render(oldRender: () => void) {
-//   try {
-//     dRoutes = await renderBefore();
-//     const pathname = history?.location?.pathname;
-//     if (pathname) {
-//       const findObject = dRoutes.find((e) => e.path === pathname);
-//       if (!findObject) {
-//         auth.fail();
-//       }
-//     } else {
-//       auth.fail();
-//     }
-//     oldRender();
-//   } catch (error) {
-//     console.log(error);
-//     oldRender();
-//   }
-// }
-
-// export { getKeepAlive };
 
 const titleList: TitleListItem[] = [
   {
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,65 +1,28 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { withRouter } from 'react-router';
 import { findPageInstById } from '@lingxiteam/engine-mobile/es/service/page';
-import { DynamicPage, lcdpApi } from '@lingxiteam/engine-mobile';
-import { Toast, Button } from 'antd-mobile';
-import VConsole from 'vconsole';
-
-interface HomePageProps {}
-
-export type validateFieldsAndShowErrorParams = (e: any) => void;
-
-export interface ForwardRefDynamicPage {
-  validateAllForms: () => Promise<any>;
-  validateFieldsAndShowError: (e: validateFieldsAndShowErrorParams) => void;
-  [key: string]: any;
-}
+import { DynamicPage, ForwardRefDynamicPage } from '@lingxiteam/engine-mobile';
+import { Button } from 'antd-mobile';
 
 const DPage = (props) => {
-  const { query = {} } = props.location;
-  const { defaultProps } = props.route;
   const [pageInst, setPageInst] = useState();
-  const { pageId = '845847207970054144' } = defaultProps || {};
-  const debugRef = useRef<any>();
+  const pageId = '845847207970054144';
+  const appId = '772790966277644288';
   const dynamicRef = useRef<ForwardRefDynamicPage>();
   const requestPageInst = () => {
     // 请求一次当前页面的具体数据
-    if (pageId) {
-      if (window.ignoreLoginPageIds?.includes(pageId)) {
-        window.ignoreLoginFlag = 'T';
+    findPageInstById({ pageId }).then(async (r) => {
+      if (!r) {
+        console.error(`没有查询到ID：${pageId}的页面实例数据，该页面将不会渲染！`);
+        return;
       }
-      findPageInstById({ pageId }).then(async (r) => {
-        if (!r) {
-          Toast.fail(`没有查询到ID:${pageId}`);
-          console.error(`没有查询到ID：${pageId}的页面实例数据，该页面将不会渲染！`);
-          return;
-        }
-        setPageInst(r);
-      });
-    } else {
-      console.error('该页面没有对应的pageId配置，将不会渲染！');
-    }
+      setPageInst(r);
+    });
   };
 
-  // useEffect(requestPageInst, [props.pageId]);
-
   useEffect(() => {
-    window.appId = '772790966277644288';
-    const setPageNavBar = lcdpApi.getRefs('setPageNavBar');
-    if (setPageNavBar) {
-      setPageNavBar({
-        pagePath: props.location.pathname,
-        navBar: {
-          hideNavBar: true,
-        },
-      });
-    }
+    // 这里为了内部接口使用，自己掉接口无需关注
+    window.appId = appId;
     requestPageInst();
-    return () => {
-      if (debugRef.current) {
-        debugRef.current.destroy();
-      }
-    };
   }, []);
 
   return (
@@ -68,18 +31,16 @@ const DPage = (props) => {
         {...props}
         ref={dynamicRef}
         pageInst={pageInst}
-        pageId={pageId || query.pageId}
-        appId={772790966277644288}
+        pageId={pageId}
+        appId={appId}
         pageDidLoad={(obj) => {
-          if (obj.debug) {
-            debugRef.current = new VConsole();
-          }
+          // 加载完成
         }}
       />
       <Button
         onClick={() => {
-          dynamicRef.current?.validateFieldsAndShowError?.((a) => {
-            console.log(a);
+          dynamicRef.current?.validateFieldsAndShowError((a) => {
+            console.log(a); // 表单调用回调
           });
         }}
       >
